f98a7421e96f2d3efd25879820cb304285c15ba3
NivBar2000 <bar.niv2000@gmail.com>
2026-04-18 16:55:39 +0300

    all data server functions integrated, and security features added

--- models.py
+++ models.py
@@ -1,57 +1,57 @@
-import uuid
+# from db import db
 
-tasks= [
-{
-    "id" : str(uuid.uuid4()),
-    "title":"learn flask",
-    "completed": False
-},
+# tasks= [
+# {
+#     "id" : str(uuid.uuid4()),
+#     "title":"learn flask",
+#     "completed": False
+# },
 
-{
-    "id" : str(uuid.uuid4()),
-    "title":"build API",
-    "completed": False
-},
+# {
+#     "id" : str(uuid.uuid4()),
+#     "title":"build API",
+#     "completed": False
+# },
 
-{
-    "id" : str(uuid.uuid4()),
-    "title":"Test with postman",
-    "completed": True
-}
-]
+# {
+#     "id" : str(uuid.uuid4()),
+#     "title":"Test with postman",
+#     "completed": True
+# }
+# ]
 
-def get_all_tasks():
-    return tasks
+# def get_all_tasks():
+    # return tasks
 
-def get_task_by_id(task_id):
-    for task in tasks:
-        if task["id"] == task_id:
-            return task
-    return None
+# def get_task_by_id(task_id):
+#      for task in tasks:
+#          if task["id"] == task_id:
+#              return task
+#      return None
         
-def create_task(task_data):
-    new_task = {
-        "completed" : False,
-        "id" : str(uuid.uuid4()),
-        "title" : task_data["title"].strip()
-    }
-    tasks.append(new_task)
-    return new_task
+# def create_task(task_data):
+#     new_task = {
+#         "id": str(uuid.uuid4()),
+#         "completed" : False,
+#         "title" : task_data["title"].strip()
+#     }
+#     db.tasks.insert_one(new_task)
+#     return new_task
 
-def update_task(body,task_id):
+# def update_task(body,task_id):
     
-    for task in tasks:
-        if task["id"] == task_id:
-            if "title" in body:
-                task["title"] = body["title"]
-            if "completed" in body:
-                task["completed"] = body["completed"]
-            return task
-    return None
+# for task in tasks:
+#     if task["id"] == task_id:
+#         if "title" in body:
+#             task["title"] = body["title"]
+#         if "completed" in body:
+#             task["completed"] = body["completed"]
+#         return task
+# return None
 
-def delete_task(task_id):
-    for task in tasks:
-        if task["id"] == task_id:
-           tasks.remove(task)
-           return tasks
-    return None
+# def delete_task(task_id):
+#     for task in tasks:
+#         if task["id"] == task_id:
+#            tasks.remove(task)
+#            return tasks
+#     return None
--- routes.py
+++ routes.py
@@ -1,29 +1,47 @@
 from flask import jsonify,request,Blueprint
 from werkzeug.exceptions import NotFound, BadRequest, Conflict, UnprocessableEntity
-from models import get_all_tasks, get_task_by_id,create_task,update_task,delete_task, tasks
-import uuid
+# from models import get_all_tasks, get_task_by_id,update_task,delete_task, tasks
+from db import db
+from bson import ObjectId
 
 tasks_bp = Blueprint("tasks", __name__)
 
 
 
-
+#========== first page =============
 @tasks_bp.route("/")
 def home():
     return jsonify({
         "message" : "Welcome to my API"
     })
- 
+
+#============ show all tasks ===========
 @tasks_bp.route("/tasks", methods =["GET"])
 def get_tasks():
-    return jsonify(get_all_tasks())
+    collection_tasks = db["tasks"]
+    tasks = list(collection_tasks.find())
+    
+    for task in tasks:
+        task["_id"] = str(task["_id"])
+    return jsonify(tasks)
+
+#============ finde task by ID =============
        
 @tasks_bp.route("/tasks/<task_id>")         
 def get_task_from_id(task_id):
-    task = get_task_by_id(task_id)
-    if task == None:
-        raise NotFound(f"Task with id {task_id} not found")
-    return(task)
+    if ObjectId.is_valid(task_id) == False:
+        raise BadRequest("ID must be vaild ObjectId ")
+    
+    collection_tasks = db["tasks"]
+    tasks = list(collection_tasks.find())
+    for task in tasks:
+        task["_id"] = str(task["_id"])
+        
+    for task in tasks:
+        if task["_id"] == task_id:
+            return task
+    raise NotFound(f"Task with id -{task_id}- not found")
+ 
        
        
 #==============Create Task=====================       
@@ -35,7 +53,7 @@ def create_task_route():
         raise BadRequest("Missing JSON body")
     if body is None:
         raise BadRequest("Missing JSON body")
-
+    
     if body == {}:
         raise BadRequest("Request body must be full JSON")
     
@@ -49,50 +67,89 @@ def create_task_route():
     if "title" not in body:
         raise BadRequest("title is required")
     
-    return jsonify(create_task(body)),201
+    new_task = {
+        "title": title.strip(),
+        "completed": False
+    }
+    
+    db.tasks.insert_one(new_task)
+    
+    new_task["_id"] = str(new_task["_id"])
+    return jsonify({
+        "success": True,
+        "data": new_task
+    }), 201
+    # return jsonify(new_task),201
 
 
 #===============Update Task by ID=====================
 
-@tasks_bp.route("/tasks/<task_id>", methods= ["PUT"])    #! בעיה בפונקציה לא עובד כמו שצריך
+@tasks_bp.route("/tasks/<task_id>", methods= ["PUT"])    
 def update(task_id):
     body = request.get_json(silent=True)
     
+    if ObjectId.is_valid(task_id) == False:
+        raise BadRequest("ID must be vaild ObjectId ")
+     
+    is_exist = db.tasks.find_one(
+            {"_id": ObjectId(task_id)}
+        )
+    if is_exist == None:
+        raise NotFound(f"Task with id -{task_id}- not found")
+
     if body is None:
         raise BadRequest("Missing JSON body")
    
-   
-    # if body == {}:
-    #     raise BadRequest("Request body must be full JSON")
     title = body.get("title")
     if title:
         if not isinstance(title, str):
             raise BadRequest("Title must be a string")
     
-    
     completed = body.get("completed")
     if completed:
         if not isinstance(completed, bool):
             raise BadRequest("completed must be bool.")
+                  
+    
+    if "title" in body:
+        new_title= body["title"]
+        db.tasks.update_one(
+            {"_id": ObjectId(task_id)},
+            {"$set": {"title":new_title}}
+        )
+    
+    if "completed" in body:
+        new_completed= body["completed"]
+        db.tasks.update_one(
+            {"_id": ObjectId(task_id)},
+            {"$set": {"completed":new_completed}}
+        )
+    
+    collection_tasks = db["tasks"]
+    tasks = list(collection_tasks.find())
+    for task in tasks:
+        task["_id"] = str(task["_id"])
         
-    updated_task = update_task(body, task_id)
-
-    if updated_task is None:
-        raise NotFound(f"Task with id {task_id} not found")
+    for task in tasks:
+        if task["_id"] == task_id:
+            return task
     
-    return jsonify(updated_task)
-
-    
-           
-            
+  
+                  
     
 @tasks_bp.route("/tasks/<task_id>", methods= ["DELETE"])
 def delete(task_id):
-    deleted_task= delete_task(task_id)
-    if deleted_task == None:
-        raise NotFound(f"Task with id {task_id} not found")
+    if ObjectId.is_valid(task_id) == False:
+        raise BadRequest("ID must be vaild ObjectId ")
     
-    return jsonify(deleted_task)
+    deleted_task = db.tasks.find_one_and_delete({"_id": ObjectId(task_id)})
+    
+    if deleted_task == None:
+        raise NotFound("Task with id -{task_id}- not found")
+    return "task deleted successfully "
+    
+  
+
     
     
     
